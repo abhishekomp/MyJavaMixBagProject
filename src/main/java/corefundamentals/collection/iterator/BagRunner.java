15f4b29f7297f36290fa69b886924cef6448461b
abhishekomp <abhishek2283@hotmail.com>
2023-10-16 21:45:22 +0200

    demo iterator obtained from the Bag object

--- src/main/java/corefundamentals/collection/iterator/BagRunner.java
+++ src/main/java/corefundamentals/collection/iterator/BagRunner.java
@@ -1,5 +1,7 @@
 package corefundamentals.collection.iterator;
 
+import java.util.Iterator;
+
 public class BagRunner {
     public static void main(String[] args) {
         Bag<String> myBag = new Bag<>();
@@ -10,8 +12,14 @@ public class BagRunner {
         myBag.add("Iterator");
         myBag.add("implementation");
 
-        /*  Just that Bag class implements Iterable and has an Iterator object,
-            we can use the for loop on myBag object just like we can use for loop on the List objects.
+        //getting the iterator from the myBag object and iterating over the items in the bag
+        Iterator<String> iterator = myBag.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+        /*  Since the Bag class implements Iterable and has an Iterator object,
+            we can use an enhanced for loop on myBag object in the same way we use for loop on the List (ArrayList) object
          */
         for(String str: myBag) {
             System.out.println(str);
